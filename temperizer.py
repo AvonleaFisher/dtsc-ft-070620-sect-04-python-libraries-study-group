6aeeeaafaab96afb09e7cfbd21ea2a10811d382c
AvonleaFisher <60533707+AvonleaFisher@users.noreply.github.com>
2020-07-15 13:52:51 -0400

    Update temperizer.py

--- temperizer.py
+++ temperizer.py
@@ -1,5 +1,5 @@
-"""An example library for converting temperatures."""
-
+def convert_c_to_f(temperature_c):
+    return (temperature_c * (9/5) + 32) 
 
 def convert_f_to_c(temperature_f):
     """Convert Fahrenheit to Celsius."""
@@ -9,29 +9,34 @@ def convert_f_to_c(temperature_f):
 
 def convert_c_to_f(temperature_c):
     """Convert Celsius to Fahrenheit."""
-    ## Add your code here
-    pass
-
 
 ## CREATE THE ADDITIONAL FUNCTIONS BELOW
 
 
 # convert_c_to_k
-
+def convert_c_to_k(temp_c):
+    return temp_c + 273.15
 
 
 # convert_f_to_k
-
-
+def convert_f_to_k(temp_f):
+    return (temp_f - 32) * (5/9) + 273.15
 
 # convert_k_to_c
-
-
+def convert_k_to_c(temp_k):
+    return temp_k - 273.15
 
 # convert_k_to_f
-
+def convert_k_to_f(temp_k):
+    return (temp_k - 273.15) * (9/5) + 32
 
 
 ## LEVEL UP
+
+# convert_f_to_all
+def convert_f_to_all(temp_f):
+    print("The temperature in {} f is:".format(temp_f))
+    print(str(f_to_c(temp_f)) + " " + "in celcius")
+    print(str(convert_f_to_k(temp_f)) + " " + "in kelvins")
 
 # convert_f_to_all
